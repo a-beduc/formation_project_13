64fdfaf638209e3f0da587614a71eaed4f3c764b
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-07-30 19:10:51 +0200

    misc: db moved to dir data

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data/oc-lettings-site.sqlite3'),
     }
 }
 
